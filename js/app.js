b58f2092ea9b81271da083d4a686dc2ef0f922a5
ayoung90 <ayoung90@live.com.au>
2023-05-27 20:22:59 +1000

    create basic math operators

--- js/app.js
+++ js/app.js
@@ -1,10 +1,59 @@
+const ERROR_DIVIDE_BY_0 = "Excuse me! Dividing by 0 hurts :(";
+
 function helloWorld() {
   console.log("Hello world");
   return 0;
 }
 
+/**
+ *
+ * @param {Number} a
+ * @param {Number} b
+ * @returns Number - result of a + b
+ */
+function add(a, b) {
+  return a + b;
+}
+
+/**
+ *
+ * @param {Number} a
+ * @param {Number} b
+ * @returns Number - result of a - b
+ */
+function subtract(a, b) {
+  return a - b;
+}
+
+/**
+ *
+ * @param {Number} a
+ * @param {Number} b
+ * @returns Number - result of a * b
+ */
+function multiply(a, b) {
+  return a * b;
+}
+
+/**
+ *
+ * @param {Number} a
+ * @param {Number} b
+ * @returns Number - result of a / b
+ */
+function divide(a, b) {
+  //handle divide by 0.
+  if (b === 0) return ERROR_DIVIDE_BY_0;
+
+  return a / b;
+}
+
 try {
   module.exports = {
-    helloWorld,
+    ERROR_DIVIDE_BY_0,
+    add,
+    subtract,
+    multiply,
+    divide,
   };
 } catch (error) {}
--- js/test/app.test.js
+++ js/test/app.test.js
@@ -1,5 +1,37 @@
-const app = require('../app');
+const app = require("../app");
 
-test('confirms jest works', () => {
-    expect(app.helloWorld()).toBe(0);
+describe("add", () => {
+  test("adds two numbers", () => {
+    expect(app.add(1, 2)).toBe(3);
   });
+  test("adds two negative numbers", () => {
+    expect(app.add(-5, -3)).toBe(-8);
+  });
+  test("adds negative and positive numbers", () => {
+    expect(app.add(4, -4)).toBe(0);
+  });
+});
+
+describe("subtract", () => {
+  test("subtracts number from another", () => {
+    expect(app.subtract(10, 4)).toBe(6);
+  });
+  test("adds two negative numbers", () => {
+    expect(app.subtract(0, 3)).toBe(-3);
+  });
+});
+
+describe("multiply", () => {
+  test("multiply two numbers", () => {
+    expect(app.multiply(6, 77)).toBe(462);
+  });
+});
+
+describe("divide", () => {
+  test("adds two numbers", () => {
+    expect(app.divide(10, 2)).toBe(5);
+  });
+  test("handle divide by 0", () => {
+    expect(app.divide(10, 0)).toBe(app.ERROR_DIVIDE_BY_0);
+  });
+});
